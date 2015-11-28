34c70386c70f41ff2963ad937538e535dcb9bd16
Penguinshmirtz <penguinshmirtz@gmail.com>
2015-11-28 15:37:18 -0600

    Began work on Battleship

---
+++ src/battleShip/PlayBattleShip.java
@@ -0,0 +1,14 @@
+package battleShip;
+
+public class PlayBattleShip {
+	public static void main(){
+
+		ArgsProcessor ap = new ArgsProcessor(args);
+		int[][]shipBoardP1 = new int[10][10];
+		int[][]shipBoardP2 = new int[10][10];
+		int[][]fireBoardP1 = new int[10][10];
+		int[][]fireBoardP2 = new int[10][10];
+	}
+
+
+}
