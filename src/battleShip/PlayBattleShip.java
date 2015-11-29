0d0ad410c332da7767480cbb16e7324c46e7623c
Neil Tomala <penguinshmirtz@gmail.com>
2015-11-29 15:16:36 -0600

    We began work on a program that plays a random Battleship game, and determines the winner.

--- src/battleShip/PlayBattleShip.java
+++ src/battleShip/PlayBattleShip.java
@@ -2,13 +2,40 @@ package battleShip;
 
 public class PlayBattleShip {
 	public static void main(){
-
-		ArgsProcessor ap = new ArgsProcessor(args);
+		//The main method will play a random game of Battleship, and determine the winner of the game
 		int[][]shipBoardP1 = new int[10][10];
 		int[][]shipBoardP2 = new int[10][10];
 		int[][]fireBoardP1 = new int[10][10];
 		int[][]fireBoardP2 = new int[10][10];
+		placeShips(shipBoardP1);
+		placeShips(shipBoardP2);
+		
 	}
 
+	public static int[][] placeShips(int[][] shipBoard){
+		int orientation = 0;
+		int shipLength = 0;
+		int randomRow = 0;
+		int randomColumn = 0;
+		for(double i = 2.4; i < 5.5; i = i + 0.7){
+			orientation = (int) (Math.random()*4);
+			shipLength = (int) i;
+			if (orientation%2 == 0){
+				randomColumn = (int) (Math.random()*10);
+				randomRow = (int) (Math.random() * (11-shipLength));
+				if (orientation == 0){
+					randomRow = randomRow + shipLength - 1;
+				}
+			} else {
+				randomRow = (int) (Math.random()*10);
+				randomColumn = (int) (Math.random() * (11-shipLength));
+				if (orientation == 3){
+					randomColumn = randomColumn + shipLength - 1;
+				}
+			}
+		
+		}
+			
+	}
 
 }
