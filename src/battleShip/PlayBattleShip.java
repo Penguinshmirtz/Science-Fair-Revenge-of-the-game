72298f9edd0613f034e4d37905bc65246d8ecc2b
Neil Tomala <penguinshmirtz@gmail.com>
2015-12-02 18:30:35 -0600

    We have finished the method to place the ships on the board in a random arrangement.

--- src/battleShip/PlayBattleShip.java
+++ src/battleShip/PlayBattleShip.java
@@ -7,19 +7,29 @@ public class PlayBattleShip {
 		int[][]shipBoardP2 = new int[10][10];
 		int[][]fireBoardP1 = new int[10][10];
 		int[][]fireBoardP2 = new int[10][10];
-		placeShips(shipBoardP1);
-		placeShips(shipBoardP2);
-		
+		shipBoardP1=placeShips();
+		shipBoardP2=placeShips();
+
 	}
 
-	public static int[][] placeShips(int[][] shipBoard){
+	/*
+	 * placeShips places ships in a random place and orientation on the board
+	 */
+	public static int[][] placeShips(){
+		int[][]shipBoard = new int[10][10];
 		int orientation = 0;
 		int shipLength = 0;
+		int shipType = 0;
 		int randomRow = 0;
 		int randomColumn = 0;
 		for(double i = 2.4; i < 5.5; i = i + 0.7){
-			orientation = (int) (Math.random()*4);
+			orientation = (int) (Math.random()*4);// 0 is north, 1 is east, 2 is south, and 3 is west
 			shipLength = (int) i;
+			if (Math.abs(i-3.1) < 0.00001) {
+				shipType = 6;
+			} else {
+				shipType = (int) i;
+			}
 			if (orientation%2 == 0){
 				randomColumn = (int) (Math.random()*10);
 				randomRow = (int) (Math.random() * (11-shipLength));
@@ -32,10 +42,30 @@ public class PlayBattleShip {
 				if (orientation == 3){
 					randomColumn = randomColumn + shipLength - 1;
 				}
+				
 			}
-		
+			if (orientation == 0) { // place the ship from row, colum to row - shiplentgh + 1, column
+				for(int j = randomRow; j > randomRow - shipLength; j--){
+					shipBoard[j][randomColumn] = shipType;
+				}
+			}
+			if (orientation == 1) { // place the ship from row, column to row, column - shiplength + 1
+				for(int j = randomColumn; j > randomColumn - shipLength; j--){
+					shipBoard[randomRow][j] = shipType;
+				}
+			}
+			if (orientation == 2) { // place the ship from row, column to row + shiplength -1, column 
+				for(int j = randomRow; j < randomRow + shipLength; j++){
+					shipBoard[j][randomColumn] = shipType;
+				}
+			}
+			if (orientation == 3) { // place the ship from row, colum to row, column + shiplength -1
+				for(int j = randomColumn; j < randomColumn + shipLength; j++){
+					shipBoard[randomRow][j] = shipType;
+				}
+			}
 		}
-			
+		return shipBoard;
 	}
 
 }
